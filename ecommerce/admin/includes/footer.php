6b66fc5768ddba2bdd1d40e744178544c69ba6e3
EMELDAS <kandjiemeldas093@gmail.com>
2022-12-06 12:39:07 +0100

    ecom 1

---
+++ ecommerce/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2012 Brought To You By<a>Emèldas Kandji</a></strong>
+</footer>
